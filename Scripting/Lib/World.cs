4e34c27474025cbada31e393e6264ea688144fa8
xrandox <xrandox17@yahoo.com>
2023-09-08 16:24:13 -0500

    Added GetMarker(s)WithinDistance

--- Scripting/Lib/World.cs
+++ Scripting/Lib/World.cs
@@ -7,15 +7,17 @@ using Neo.IronLua;
 using TmfLib.Pathable;
 using Attribute = TmfLib.Prototype.Attribute;
 
-namespace BhModule.Community.Pathing.Scripting.Lib; 
+namespace BhModule.Community.Pathing.Scripting.Lib;
 
-public class World {
+public class World
+{
 
     private readonly Dictionary<string, Guid> _guidCache = new(StringComparer.InvariantCultureIgnoreCase);
 
     private readonly PathingGlobal _global;
 
-    internal World(PathingGlobal global) {
+    internal World(PathingGlobal global)
+    {
         _global = global;
     }
 
@@ -23,21 +25,26 @@ public class World {
 
     public PathingCategory RootCategory => _global.ScriptEngine.Module.PackInitiator.PackState.RootCategory;
 
-    public PathingCategory CategoryByType(string id) {
+    public PathingCategory CategoryByType(string id)
+    {
         return _global.ScriptEngine.Module.PackInitiator.PackState.RootCategory.TryGetCategoryFromNamespace(id, out var category)
-                   ? category 
+                   ? category
                    : null;
     }
 
     // Pathables
 
-    public IPathingEntity PathableByGuid(string guid) {
-        if (!_guidCache.TryGetValue(guid, out var g)) {
+    public IPathingEntity PathableByGuid(string guid)
+    {
+        if (!_guidCache.TryGetValue(guid, out var g))
+        {
             _guidCache.Add(guid, g = AttributeParsingUtil.InternalGetValueAsGuid(guid));
         }
 
-        foreach (var pathable in _global.ScriptEngine.Module.PackInitiator.PackState.Entities) {
-            if (pathable.Guid == g) {
+        foreach (var pathable in _global.ScriptEngine.Module.PackInitiator.PackState.Entities)
+        {
+            if (pathable.Guid == g)
+            {
                 return pathable;
             }
         }
@@ -45,15 +52,19 @@ public class World {
         return null;
     }
 
-    public LuaTable PathablesByGuid(string guid) {
+    public LuaTable PathablesByGuid(string guid)
+    {
         var nTable = new LuaTable();
 
-        if (!_guidCache.TryGetValue(guid, out var g)) {
+        if (!_guidCache.TryGetValue(guid, out var g))
+        {
             _guidCache.Add(guid, g = AttributeParsingUtil.InternalGetValueAsGuid(guid));
         }
 
-        foreach (var pathable in _global.ScriptEngine.Module.PackInitiator.PackState.Entities) {
-            if (pathable.Guid == g) {
+        foreach (var pathable in _global.ScriptEngine.Module.PackInitiator.PackState.Entities)
+        {
+            if (pathable.Guid == g)
+            {
                 nTable.Add(pathable);
             }
         }
@@ -63,17 +74,20 @@ public class World {
 
     // Markers
 
-    public StandardMarker MarkerByGuid(string guid) {
-        return PathableByGuid(guid) is StandardMarker marker 
-                   ? marker 
+    public StandardMarker MarkerByGuid(string guid)
+    {
+        return PathableByGuid(guid) is StandardMarker marker
+                   ? marker
                    : null;
     }
 
-    public StandardMarker GetClosestMarker() {
+    public StandardMarker GetClosestMarker()
+    {
         return _global.ScriptEngine.Module.PackInitiator.PackState.Entities.ToArray().OfType<StandardMarker>().OrderBy(marker => marker.DistanceToPlayer).FirstOrDefault();
     }
 
-    public StandardMarker GetClosestMarker(PathingCategory category) {
+    public StandardMarker GetClosestMarker(PathingCategory category)
+    {
         return _global.ScriptEngine.Module.PackInitiator.PackState.Entities.ToArray()
                       .OfType<StandardMarker>()
                       .Where(marker => marker.Category == category)
@@ -81,24 +95,28 @@ public class World {
                       .FirstOrDefault();
     }
 
-    public LuaTable GetClosestMarkers(int quantity) {
+    public LuaTable GetClosestMarkers(int quantity)
+    {
         var nTable = new LuaTable();
 
-        foreach (var marker in _global.ScriptEngine.Module.PackInitiator.PackState.Entities.ToArray().OfType<StandardMarker>().OrderBy(marker => marker.DistanceToPlayer).Take(quantity)) {
+        foreach (var marker in _global.ScriptEngine.Module.PackInitiator.PackState.Entities.ToArray().OfType<StandardMarker>().OrderBy(marker => marker.DistanceToPlayer).Take(quantity))
+        {
             nTable.Add(marker);
         }
 
         return nTable;
     }
 
-    public LuaTable GetClosestMarkers(PathingCategory category, int quantity) {
+    public LuaTable GetClosestMarkers(PathingCategory category, int quantity)
+    {
         var nTable = new LuaTable();
 
         foreach (var marker in _global.ScriptEngine.Module.PackInitiator.PackState.Entities.ToArray()
                                       .OfType<StandardMarker>()
                                       .Where(marker => marker.Category == category)
                                       .OrderBy(marker => marker.DistanceToPlayer)
-                                      .Take(quantity)) {
+                                      .Take(quantity))
+        {
             nTable.Add(marker);
         }
 
@@ -157,7 +175,8 @@ public class World {
 
     // Trails
 
-    public StandardTrail TrailByGuid(string guid) {
+    public StandardTrail TrailByGuid(string guid)
+    {
         return PathableByGuid(guid) is StandardTrail trail
                    ? trail
                    : null;
